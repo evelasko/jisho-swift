ce522e4288e46048cc8fb7fa5a5da15aee535414
velasko <h.superpotter@gmail.com>
2020-07-25 13:04:44 +0200

    Downgraded to Swift 5.2 conforming to Kana Kun release

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,29 +14,25 @@ let package = Package(
             targets: ["jisho-swift"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(
             name: "wanakana-swift",
             url: "https://github.com/evelasko/wanakana-swift.git",
-            from: "0.0.1"
+            .upToNextMajor(from: "0.0.5")
         )
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "jisho-swift",
             dependencies: [
                 "wanakana-swift"
             ]),
-        .testTarget(
-            name: "jisho-swiftTests",
-            dependencies: ["jisho-swift"],
-            resources: [
-                .process("JishoEntry.json"),
-                .process("JishoJSONResponse.json")
-            ]
-            ),
+//        .testTarget(
+//            name: "jisho-swiftTests",
+//            dependencies: ["jisho-swift"],
+//            sources: [
+//                .process("JishoEntry.json"),
+//                .process("JishoJSONResponse.json")
+//            ]
+//            ),
     ]
 )
